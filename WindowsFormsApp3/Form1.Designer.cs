e3fa5b569e69f2c1961a643c48404333b6397f6f
S.Farruhbek <satimbayevfarruhbek@gmail.com>
2024-04-16 00:24:08 +0500

    Add project files.

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.txtM = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtN
+            // 
+            this.txtN.Location = new System.Drawing.Point(283, 83);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(204, 20);
+            this.txtN.TabIndex = 0;
+            this.txtN.TextChanged += new System.EventHandler(this.txtN_TextChanged);
+            // 
+            // txtM
+            // 
+            this.txtM.Location = new System.Drawing.Point(283, 125);
+            this.txtM.Name = "txtM";
+            this.txtM.Size = new System.Drawing.Size(204, 20);
+            this.txtM.TabIndex = 1;
+            this.txtM.TextChanged += new System.EventHandler(this.txtM_TextChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(62, 255);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(678, 166);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(275, 220);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(336, 160);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 37);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Natija";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtM);
+            this.Controls.Add(this.txtN);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtN;
+        private System.Windows.Forms.TextBox txtM;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtN_TextChanged(object sender, EventArgs e)
+        {
+            //
+        }
+
+        private void txtM_TextChanged(object sender, EventArgs e)
+        {
+            //
+        }
+
+        private void lblResult_Click(object sender, EventArgs e)
+        {
+            //
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //
+        }
+
+
+        private void FillArrayAndDisplay()
+        {
+            if (!int.TryParse(txtN.Text, out int n) || !int.TryParse(txtM.Text, out int m))
+            {
+                MessageBox.Show("N va M sonlarini to'g'ri kiriting.");
+                return;
+            }
+
+            if (n <= 0 || m <= 0)
+            {
+                MessageBox.Show("N va M musbat bo'lishi kerak.");
+                return;
+            }
+
+            Random random = new Random();
+            int[,] array = new int[n, m];
+            int countGreaterThan200 = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    array[i, j] = random.Next(-3000, 3001);
+                    if (array[i, j] > 200)
+                    {
+                        countGreaterThan200++;
+                    }
+                }
+            }
+
+            dataGridView1.Columns.Clear();
+
+            for (int j = 0; j < m; j++)
+            {
+                dataGridView1.Columns.Add($"Column{j + 1}", $"Column {j + 1}");
+            }
+
+            dataGridView1.Rows.Clear();
+            for (int i = 0; i < n; i++)
+            {
+                dataGridView1.Rows.Add();
+                for (int j = 0; j < m; j++)
+                {
+                    dataGridView1[j, i].Value = array[i, j];
+                }
+            }
+
+
+            lblResult.Text = $"Qiymati 200 dan katta elementlar soni: {countGreaterThan200}";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FillArrayAndDisplay();
+        }
+    }
+}
